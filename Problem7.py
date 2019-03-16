9b8ef9b1e224297853816e1d609e1be5ba9825f0
Celeste Quinlan <G00376296@gmit.ie>
2019-03-16 16:11:38 +0000

    need to change to floating point number

---
+++ Problem7.py
@@ -0,0 +1,12 @@
+#**************************
+# Created by Catherine Ann Celeste Quinlan
+# This program will take a FLOATING POINT NUMBER as input and output its square root approximation.
+# QUESTION 7
+#*************************
+
+import math
+
+
+Selectednumber=input ("Please enter a positive number  :  " )
+number =int(Selectednumber)
+print(math.sqrt(number)) 
