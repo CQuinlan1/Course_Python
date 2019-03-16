a30935fa34ad8ab968516d1466f99b0c439fb2e2
Celeste Quinlan <G00376296@gmit.ie>
2019-03-16 15:52:21 +0000

    work in progress

--- Problem4.py
+++ Problem4.py
@@ -9,13 +9,16 @@
 Selectednumber=input ("Please enter a positive integer number  :  " )
 number =int(Selectednumber)
 
-if(number > 0) :
+if(number > 0):
     print("User number is positive ", Selectednumber)
     if (number == 1):
         print("Please pick a bigger number than 1 to run program")
     
 else: 
-    print ("your number is negative" )
-while (number > 0) and (number!=1)
-number % 2 == 0 :
-    number=number / 2
+    print ("your number is negative")
+while (number > 0) and (number!=1):
+    if number % 2 == 0 :
+        number= number / 2
+else:
+    number = (number *3) +1
+
